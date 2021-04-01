a94eaa4e8447e983ca3fab7f7bf8c3d059023c32
Hyunseung Lee <59816802+ceuity@users.noreply.github.com>
2021-04-02 00:39:07 +0900

    Update app.py

--- app.py
+++ app.py
@@ -23,24 +23,6 @@ def healthCheck():
 	return "", 200
 
 
-# @app.route("/upload_image", methods=["POST"])
-# def upload_image_file():
-#     if request.method == 'POST':
-#         width, height = 256, 256
-#         file = request.files['uploaded_image']
-#         if not file:
-#             return render_template('index_test.html', label="No Files")
-
-#         source = file.read()
-#         adjusted_image = preprocess(source, width, height)
-#         image = denormalize(adjusted_image)
-#         image = Image.fromarray(image)
-#         image_path = './images/result_image.png'
-#         image.save(image_path)
-
-#         return render_template('index_test.html', label=image_path)
-
-
 @app.route("/images", methods=['POST'])
 def get_result():
 	if request.method == "POST":
@@ -60,8 +42,7 @@ def get_result():
 		except Exception as e:
 			print("error : %s" % e)
 			return Response("fail", status=400)
-	# return Response("success", status=200)
 
-
+		
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port='80')
